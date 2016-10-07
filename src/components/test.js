36fffd07ecf427d441556a252ea5cfa9c35cc08f
Zenaro <572001239@qq.com>
2016-10-07 14:42:47 +0800

    init again

---
+++ src/components/test.js
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var test = React.createClass({
+
+	render: function() {
+		return (
+			<div>
+				<span>test</span>
+			</div>
+		);
+	}
+});
+
+module.exports = test;
