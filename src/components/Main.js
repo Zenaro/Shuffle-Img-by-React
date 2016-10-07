9c34b0e37682981b5e1100459781fe938aa61952
Zenaro <572001239@qq.com>
2016-10-07 15:39:21 +0800

    success in first step

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,36 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/App.less');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+// 获取图片相关的数据
+let imagesData = require('../data/imagesData.json');
+
+// 自执行函数，将图片名信息转成图片URL路径信息
+imagesData = (function genImageURL(imagesDataArr) {
+	for (let i = 0, j = imagesDataArr.length; i < j; i++) {
+		let singleImageData = imagesDataArr[i];
+
+		singleImageData.imageURL = require('../images/' +
+			singleImageData.fileName);
+
+		imagesDataArr[i] = singleImageData;
+	}
+
+	return imagesDataArr;
+})(imagesData);
 
 class AppComponent extends React.Component {
 	render() {
 		return (
-			<div className="index">
-        		<img src={yeomanImage} alt="Yeoman Generator" />
-        		<div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      		</div>
+			<section className="stage">
+				<section className="img-sec">
+
+				</section>
+				<nav className="controller-nav">
+
+				</nav>
+			</section>
 		);
 	}
 }
